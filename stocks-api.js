0469d23bccca63e91c4fdf715e7f53ddcb3e0db8
KarlSemilla <karlsemilla1@gmail.com>
2020-03-23 21:59:05 -0600

    On 'Test your Knowledge 1'

--- stocks-api.js
+++ stocks-api.js
@@ -1,58 +1,42 @@
 // first reference required modules
-const fs = require('fs');
-const path = require('path');
-const parser = require('body-parser');
-const express = require('express');
+const path = require("path");
+const parser = require("body-parser");
+const express = require("express");
+
+// error messages need to be returned in JSON format
+const jsonMessage = msg => {
+  return { message: msg };
+};
 
 // for now, we will get our data by reading the provided json file
-const jsonPath = path.join(__dirname, 'public',
-    'stocks-complete.json');
-const jsonData = fs.readFileSync(jsonPath, 'utf8');
+const jsonPath = path.join(__dirname, "public", "stocks-complete.json");
 
 // reference our own modules
-const stocks = require('./scripts/data-provider.js');
+const stocks = require("./scripts/data-provider.js");
+const stockRouter = require("./scripts/stock-router.js");
 
 // create an express app
 const app = express();
 
 // tell node to use json and HTTP header features in body-parser
 app.use(parser.json());
-app.use(parser.urlencoded({
-    extended: true
-}));
+app.use(parser.urlencoded({ extended: true }));
 
 // handle requests for static resources
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-// return just the requested stock
-app.get('/stock/:symbol', (req, resp) => {
-    // change user supplied symbol to upper case
-    const symbolToFind = req.params.symbol.toUpperCase();
-    // search the array of objects for a match
-    const matches =
-        stocks.filter(obj => symbolToFind === obj.symbol);
-    // return the matching stock
-    resp.json(matches);
-});
-
-// return all the stocks whose name contains the supplied text
-app.get('/stock/name/:substring', (req, resp) => {
-    // change user supplied substring to lower case
-    const substring = req.params.substring.toLowerCase();
-    // search the array of objects for a match
-    const matches = stocks.filter((obj) =>
-        obj.name.toLowerCase().includes(substring));
-    // return the matching stocks
-    resp.json(matches);
-});
+// handle other requests for stocks
+stockRouter.handleSingleSymbol(stocks, app);
+stockRouter.handleNameSearch(stocks, app);
+stockRouter.handlePriceData(stocks, app);
 
 // return all the stocks when a root request arrives
 app.get((req, resp) => {
-    resp.json(stocks)
+  resp.json(stocks);
 });
 
 // Use express to listen to port
 let port = 8080;
 app.listen(port, () => {
-    console.log("Server running at port= " + port);
+  console.log("Server running at port= " + port);
 });
