ff94f0ab7d71d1b9c4997c7728d229fc9797efe0
KarlSemilla <karlsemilla1@gmail.com>
2020-03-25 21:22:36 -0600

    Completed test 1

--- scripts/data-provider.js
+++ scripts/data-provider.js
@@ -1,12 +1,18 @@
-const path = require("path");
-const fs = require("fs");
+const fetch = require("node-fetch");
+const stockRouter = require("./stock-router.js");
+async function retrieveCompanies(app) {
+  const url = `https://www.randyconnolly.com/funwebdev/3rd/api/stocks/companies.php`;
+  // use fetch and await to get stocks data
+  const response = await fetch(url);
+  const stocks = await response.json();
 
-// for now, we will get our data by reading the provided json file
-const jsonPath = path.join(__dirname, '../public',
-    'stocks-complete.json');
-const jsonData = fs.readFileSync(jsonPath, 'utf8');
-
-// convert string data into JSON object
-const stocks = JSON.parse(jsonData);
-
-module.exports = stocks;
+  // handle other requests for stocks
+  stockRouter.handleSingleSymbol(stocks, app);
+  stockRouter.handleNameSearch(stocks, app);
+  stockRouter.handlePriceData(stocks, app);
+  // return all the stocks when a root request arrives
+  app.get("/", (req, resp) => {
+    resp.json(stocks);
+  });
+}
+module.exports = { retrieveCompanies };
