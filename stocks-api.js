fb3380d1ffe88115e7121943df262a45b80deac5
KarlSemilla <karlsemilla1@gmail.com>
2020-03-28 00:20:53 -0600

    On the last test

--- stocks-api.js
+++ stocks-api.js
@@ -11,6 +11,34 @@ const provider = require("./scripts/data-provider.js");
 provider.retrieveCompanies(app);
 
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(
+  "/socket.io",
+  express.static(path.join(__dirname, "/node_modules/socket.io-client/dist/"))
+);
+
+// listen for socket communication on port 3000
+const server = require("socket.io");
+const io = new server(3000);
+
+io.on("connection", socket => {
+  console.log("new connection made with client");
+  // client has sent a new user has joined message
+  socket.on("username", msg => {
+    console.log("username: " + msg);
+    // attach passed username with this communication socket
+    socket.username = msg;
+    // broadcast message to all connected clients
+    const obj = { message: "Has joined", user: msg };
+    io.emit("user joined", obj);
+  });
+  // client has sent a chat message ... broadcast it
+  socket.on("chat from client", msg => {
+    socket.broadcast.emit("chat from server", {
+      user: socket.username,
+      message: msg
+    });
+  });
+});
 
 let port = 8080;
 app.listen(port, () => {
